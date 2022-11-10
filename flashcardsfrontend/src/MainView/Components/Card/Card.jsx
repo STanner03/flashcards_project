49b2997d16046834947f706e62565e712a0d7d1e
Shane <shanetanner03@gmail.com>
2022-11-10 10:11:33 -0600

    Added an Edit card function & an Add card function to Card jsx. Have not wrote the logic to submit to backend.'

--- flashcardsfrontend/src/MainView/Components/Card/Card.jsx
+++ flashcardsfrontend/src/MainView/Components/Card/Card.jsx
@@ -3,15 +3,30 @@ import CreateCardForm from "../CreateCardForm/CreateCardForm";
 
 const Card = ({cardData, createNewCard}) => {
 
+    const [display, setDisplay] = useState(cardData.word)
+
     function handleClick(){
+        if (display === cardData.word) {
+            setDisplay(cardData.definition)
+        } else {
+            setDisplay(cardData.word)
+        }
+    };
+
+    function handleAddCard(){
+        <CreateCardForm createNewCard={createNewCard} />
+    }
+
+    function handleEditCard() {
+        <CreateCardForm cardData={cardData} />
         
     }
 
     return ( 
         <div onClick={handleClick}>
-            <h4>{cardData.word}</h4>
-            <p>{cardData.definition}</p>
-            <CreateCardForm createNewCard={createNewCard} />
+            <button onClick={handleAddCard} >Add New Card</button>
+            <button onClick={handleEditCard} >Edit Card</button>
+            <div>{display}</div>
         </div>
      );
 }
